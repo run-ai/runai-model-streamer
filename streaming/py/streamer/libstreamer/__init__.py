40611a1a123413948acd03dd1cecb4a43710ae4c
omer-dayan <omer@run.ai>
2024-03-07 17:37:35 +0200

    Python Libstreamer DLL Initialization (#47)
    * Initialize DLL function during constructor
    
    * Rename DLLWrapper to StreamerDLLWrapper
    
    ---------
    
    Co-authored-by: Omer Dayan <omer@Admins-MacBook-Pro-3.local>

--- streaming/py/streamer/libstreamer/__init__.py
+++ streaming/py/streamer/libstreamer/__init__.py
@@ -9,36 +9,36 @@ STREAMER_LIBRARY = os.environ.get("STREAMER_LIBRARY", DEFAULT_STREAMER_LIBRARY)
 t_streamer = ctypes.c_void_p
 
 
-class DLLWrapper:
+class LibstreamerDLLWrapper:
     def __init__(self, library_path):
         self.lib = ctypes.CDLL(library_path)
 
+        self.fn_runai_start = self.lib.runai_start
+        self.fn_runai_start.argtypes = [ctypes.POINTER(t_streamer)]
+        self.fn_runai_start.restype = ctypes.c_int
 
-dll = DLLWrapper(STREAMER_LIBRARY)
+        self.fn_runai_end = self.lib.runai_end
+        self.fn_runai_end.argtypes = [t_streamer]
 
-dll.fn_runai_start = dll.lib.runai_start
-dll.fn_runai_start.argtypes = [ctypes.POINTER(t_streamer)]
-dll.fn_runai_start.restype = ctypes.c_int
+        self.fn_runai_request = self.lib.runai_request
+        self.fn_runai_request.argtypes = [
+            t_streamer,
+            ctypes.c_char_p,
+            ctypes.c_size_t,
+            ctypes.c_size_t,
+            ctypes.c_void_p,
+            ctypes.c_uint32,
+            ctypes.POINTER(ctypes.c_size_t),
+        ]
+        self.fn_runai_request.restype = ctypes.c_int
 
-dll.fn_runai_end = dll.lib.runai_end
-dll.fn_runai_end.argtypes = [t_streamer]
+        self.fn_runai_response = self.lib.runai_response
+        self.fn_runai_response.argtypes = [t_streamer, ctypes.POINTER(ctypes.c_uint32)]
+        self.fn_runai_response.restype = ctypes.c_int
 
-dll.fn_runai_request = dll.lib.runai_request
-dll.fn_runai_request.argtypes = [
-    t_streamer,
-    ctypes.c_char_p,
-    ctypes.c_size_t,
-    ctypes.c_size_t,
-    ctypes.c_void_p,
-    ctypes.c_uint32,
-    ctypes.POINTER(ctypes.c_size_t),
-]
-dll.fn_runai_request.restype = ctypes.c_int
+        self.fn_runai_response_str = self.lib.runai_response_str
+        self.fn_runai_response_str.argtypes = [ctypes.c_int]
+        self.fn_runai_response_str.restype = ctypes.c_char_p
 
-dll.fn_runai_response = dll.lib.runai_response
-dll.fn_runai_response.argtypes = [t_streamer, ctypes.POINTER(ctypes.c_uint32)]
-dll.fn_runai_response.restype = ctypes.c_int
 
-dll.fn_runai_response_str = dll.lib.runai_response_str
-dll.fn_runai_response_str.argtypes = [ctypes.c_int]
-dll.fn_runai_response_str.restype = ctypes.c_char_p
+dll = LibstreamerDLLWrapper(STREAMER_LIBRARY)
