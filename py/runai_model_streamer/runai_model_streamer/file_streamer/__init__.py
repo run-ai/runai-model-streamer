4464cccc97ce5d62808b038982ef03cdc693d462
omer-dayan <omer@run.ai>
2025-05-28 14:01:51 +0300

    Multi file python layer (#59)
    * Refactor request iterator
    
    * FilesRequestsIterator for single file
    
    * FilesRequestsIterator API multi files
    
    * Multi file python layer
    
    * CR
    
    * Remove old APIs
    
    * Code review
    
    * File streamer buffer

---
+++ py/runai_model_streamer/runai_model_streamer/file_streamer/__init__.py
@@ -0,0 +1,9 @@
+from runai_model_streamer.file_streamer.requests_iterator import (
+    FileChunks,
+)
+from runai_model_streamer.file_streamer.file_streamer import FileStreamer
+
+__all__ = [
+    "FileStreamer",
+    "FileChunks",
+]
