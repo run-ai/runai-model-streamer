f8b377edfc7248ce33bf31f448c7b4b0ac3f3c69
omer-dayan <omer@run.ai>
2025-07-01 14:08:24 +0300

    S3 tests as part of CI (#74)
    * S3 tests as part of CI
    
    * Tests timing
    
    * Change S3 init

--- tests/fuzzing/test_safetensors_streamer.py
+++ tests/fuzzing/test_safetensors_streamer.py
@@ -2,76 +2,23 @@ import unittest
 import tempfile
 import shutil
 import os
-import random
-import string
 import torch
-from safetensors.torch import save_file, safe_open
+from safetensors.torch import safe_open
+from tests.safetensors.generator import (create_random_safetensors, create_random_multi_safetensors)
+from tests.safetensors.comparison import tensor_maps_are_equal
 from runai_model_streamer.safetensors_streamer.safetensors_streamer import (
     SafetensorsStreamer,
 )
 
 MIN_NUM_FILES = 1
 MAX_NUM_FILES = 20
-MIN_NUM_TENSORS = 1
-MAX_NUM_TENSORS = 5
-MIN_TENSOR_SIZE = 16
-MAX_TENSOR_SIZE = 2048
-MIN_TENSOR_NAME_LEN = 1
-MAX_TENSOR_NAME_LEN = 248
-MIN_TENSOR_SHAPE_DIM = 1
-MAX_TENSOR_SHAPE_DIM = 10
-ST_DATA_TYPES = [
-    torch.float64,
-    torch.float32,
-    torch.float16,
-    torch.bfloat16,
-    torch.int64,
-    torch.int32,
-    torch.int16,
-    torch.int8,
-    torch.uint8,
-    torch.bool,
-]
-
-
-def random_name():
-    characters = string.ascii_letters + string.digits + "."
-    return "".join(
-        random.choice(characters)
-        for _ in range(random.randint(MIN_TENSOR_NAME_LEN, MAX_TENSOR_NAME_LEN))
-    )
-
-
-def random_tensors():
-    tensors = {}
-    for i in range(random.randint(MIN_NUM_TENSORS, MAX_NUM_TENSORS)):
-        dtype = random.shuffle(ST_DATA_TYPES)
-        shape = tuple(
-            random.randint(MIN_TENSOR_SHAPE_DIM, MAX_TENSOR_SHAPE_DIM)
-            for _ in range(random.randint(MIN_TENSOR_SHAPE_DIM, MAX_TENSOR_SHAPE_DIM))
-        )
-        name = random_name()
-
-        if dtype in [torch.int32, torch.int64]:
-            tensor = torch.randint(low=0, high=100, size=shape, dtype=dtype)
-        else:
-            tensor = torch.rand(size=shape, dtype=dtype)
-
-        tensors[name] = tensor
-    return tensors
-
-
-def create_random_safetensors(path):
-    save_file(random_tensors(), path)
-
 
 class TestSafetensorStreamerFuzzing(unittest.TestCase):
     def setUp(self):
         self.temp_dir = tempfile.mkdtemp()
 
     def test_safetensors_streamer(self):
-        file_path = os.path.join(self.temp_dir, "model.safetensors")
-        create_random_safetensors(file_path)
+        file_path = create_random_safetensors(self.temp_dir)
 
         file_size = os.path.getsize(file_path)
 
@@ -88,20 +35,12 @@ class TestSafetensorStreamerFuzzing(unittest.TestCase):
             for name in f.keys():
                 their[name] = f.get_tensor(name)
 
-        self.assertEqual(len(our.items()), len(their.items()))
-        for name, our_tensor in our.items():
-            self.assertTrue(our_tensor.is_contiguous())
-            self.assertEqual(our_tensor.dtype, their[name].dtype)
-            self.assertEqual(our_tensor.shape, their[name].shape)
-            res = torch.all(our_tensor.eq(their[name]))
-            self.assertTrue(res)
+        equal, message = tensor_maps_are_equal(our, their)
+        if not equal:
+            self.fail(f"Tensor mismatch: {message}")
 
     def test_safetensors_streamer_stream_files(self):
-        file_paths = []
-        for i in range(random.randint(MIN_NUM_FILES, MAX_NUM_FILES)):
-            file_path = os.path.join(self.temp_dir, f"model-{i}.safetensors")
-            create_random_safetensors(file_path)
-            file_paths.append(file_path)
+        file_paths = create_random_multi_safetensors(self.temp_dir)
 
         files_size = sum([os.path.getsize(file_path) for file_path in file_paths])
 
